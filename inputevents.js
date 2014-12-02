95862d4f16d4084367dbddd20e4ef45df400a836
Joshua Marinacci <joshua.marinacci@nokia.com>
2014-12-01 16:34:53 -0800

    more amino and browser updates

--- inputevents.js
+++ inputevents.js
@@ -7,18 +7,20 @@ var KEYS = {
     ENTER:'ENTER',
     BACK_DELETE:'BACK_DELETE',
     FORWARD_DELETE:'FORWARD_DELETE',
+    SHIFT:'SHIFT',
 }
 
 
 //for browser
 var browsercodes_to_keys = {
     39:KEYS.RIGHT,
-    37:'LEFT',
+    37:KEYS.LEFT,
     40:'DOWN',
     38:'UP',
     8:'BACK_DELETE',
     46:'FORWARD_DELETE',
     13:'ENTER',
+    16: KEYS.SHIFT,
 }
 
 var aminocodes_to_keys = {
@@ -29,15 +31,97 @@ var aminocodes_to_keys = {
     295: KEYS.BACK_DELETE,
     297: KEYS.FORWARD_DELETE,
     294: KEYS.ENTER,
+    287: KEYS.SHIFT,
+    288: KEYS.SHIFT,
+}
+
+
+//American keyboard
+var shift_keys = [287, 288];
+var aminoshift_map = { };
+var aminocodes_to_chars = {  32:' ', }
+var browsercodes_to_chars = { }
+var browsershift_map = { }
+exports.init = function() {
+    function insertMap(map, start, len, str) {
+        for(var i=0; i<len; i++) {
+            map[start+i] = str[i];
+        }
+    }
+    insertMap(aminoshift_map, 48,10, ')!@#$%^&*(');
+    function addKeysWithShift(start, str) {
+        if(str.length % 2 != 0) throw new Error("not an even number of chars");
+        for(var i=0; i<str.length/2; i++) {
+            aminocodes_to_chars[start+i] = str[i*2+0];
+            aminoshift_map[start+i] = str[i*2+1];
+        }
+    }
+    addKeysWithShift(186,';:=+,<-_.>/?`~');
+    addKeysWithShift(219,'[{\\|]}\'"');
+    addKeysWithShift(48,'0)1!2@3#4$5%6^7&8*9(');
+
+    //on my imac keyboard
+    aminocodes_to_chars[91] = '[';
+    aminoshift_map[91] = '{';
+    aminocodes_to_chars[93] = ']';
+    aminoshift_map[93] = '}';
+
+
+    browsercodes_to_chars[49] = '1';
+    browsershift_map[49]      = '!';
 }
 
 exports.fromBrowserKeyboardEvent = function(evt) {
-    console.log("processing browser event");
+    console.log("processing browser event",evt);
+    var shift = false;
+    shift = (evt.shift==1);
+
+    var key = browsercodes_to_keys[evt.keycode];
+    if(key) {
+        return {
+            recognized:true,
+            key:key,
+            shift:false,
+            printable:false,
+        }
+    }
+
+    if(browsercodes_to_chars[evt.keycode]) {
+        var char = browsercodes_to_chars[evt.keycode];
+        if(shift && browsershift_map[evt.keycode]) {
+            char = browsershift_map[evt.keycode];
+        }
+        return {
+            recognized:true,
+            keycode: evt.keycode,
+            printable:true,
+            char: char,
+        }
+    }
+
+    if(evt.keycode >= 65 && evt.keycode <= 90) {
+        var char = String.fromCharCode(evt.keycode);
+        if(shift) {
+            char = char.toUpperCase();
+        } else {
+            char = char.toLowerCase();
+        }
+        return {
+            recognized: true,
+            keycode: evt.keycode,
+            printable:true,
+            char: char,
+        }
+    }
 }
 
-exports.fromAminoKeyboardEvent = function(evt) {
-    //console.log("processing amino event",evt);
-    //console.log("printable = ", evt.printableChar);
+exports.fromAminoKeyboardEvent = function(evt, states) {
+
+    var shift = false;
+    for(var i=0; i<shift_keys.length; i++) {
+        if(states[shift_keys[i]] === true) shift = true;
+    }
+
     var key = aminocodes_to_keys[evt.keycode];
     if(key) {
         return {
@@ -48,7 +132,35 @@ exports.fromAminoKeyboardEvent = function(evt) {
         }
     }
 
+    if(aminocodes_to_chars[evt.keycode]) {
+        var char = aminocodes_to_chars[evt.keycode];
+        if(shift && aminoshift_map[evt.keycode]) {
+            char = aminoshift_map[evt.keycode];
+        }
+        return {
+            recognized:true,
+            keycode: evt.keycode,
+            printable:true,
+            char: char,
+        }
+    }
 
+    if(evt.keycode >= 65 && evt.keycode <= 90) {
+        var char = String.fromCharCode(evt.keycode);
+        if(shift) {
+            char = char.toUpperCase();
+        } else {
+            char = char.toLowerCase();
+        }
+        return {
+            recognized: true,
+            keycode: evt.keycode,
+            printable:true,
+            char: char,
+        }
+    }
+
+    /*
     if(evt.printable) {
         return {
             recognized:true,
@@ -56,7 +168,7 @@ exports.fromAminoKeyboardEvent = function(evt) {
             char: evt.printableChar,
             shift: evt.shift,
         }
-    }
+    }*/
 
     return {
         recognized:false,
